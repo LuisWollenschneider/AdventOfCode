585dc2d39fba0a8c54e2a6f7cfe9456c515379ca
Luis Wollenschneider <luwo00001@stud.uni-saarland.de>
2023-12-06 21:02:49 +0100

    Template for Rust solutions 🚀

---
+++ template_rust/src/main.rs
@@ -0,0 +1,20 @@
+use utils_rust::utils::wrapper;
+
+const DAY: i32 = 1;
+const YEAR: i32 = 2023;
+
+#[warn(unused_variables)]
+fn main() {
+    wrapper(part_1, YEAR, DAY, 1);
+    wrapper(part_2, YEAR, DAY, 2);
+}
+
+#[allow(unused_variables)]
+fn part_1(input: &String) -> String {
+    "".to_string()
+}
+
+#[allow(unused_variables)]
+fn part_2(input: &String) -> String {
+    "".to_string()
+}
