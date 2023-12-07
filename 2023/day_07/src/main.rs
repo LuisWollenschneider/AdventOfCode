a7fc8f053b2583636b1cc423db5d48dc3f33c8a6
Luis Wollenschneider <luwo00001@stud.uni-saarland.de>
2023-12-07 22:28:07 +0100

    blazingly fast 🚀

--- 2023/day_07/src/main.rs
+++ 2023/day_07/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use utils_rust::utils::wrapper;
 
 const DAY: i32 = 7;
@@ -11,130 +10,77 @@ fn main() {
 }
 
 struct Hand {
-    cards: Vec<u64>,
     bid: u64,
     rank: u64,
 }
 
 #[allow(unused_variables)]
 fn part_1(input: &String) -> String {
-    let mut hands: Vec<Hand> = parse_input(input, &"11".to_string());
-
-    for hand in &mut hands {
-        let mut dict: HashMap<u64, u64> = HashMap::new();
-        for card in hand.cards.iter() {
-            dict.insert(*card, dict.get(card).unwrap_or(&0) + 1);
-        }
-        // get values from dict, not keys
-        let mut freq = dict.values()
-            .map(|v| *v)
-            .collect::<Vec<u64>>();
-        freq.sort();
-        freq.reverse();
-
-        if freq[0] == 5 { // 5 of a kind
-            hand.rank = 7;
-        } else if freq[0] == 4 { // 4 of a kind
-            hand.rank = 6;
-        } else if freq[0] == 3 && freq[1] == 2 { // full house
-            hand.rank = 5;
-        } else if freq[0] == 3 { // 3 of a kind
-            hand.rank = 4;
-        } else if freq[0] == 2 && freq[1] == 2 { // 2 pairs
-            hand.rank = 3;
-        } else if freq[0] == 2 { // 1 pair
-            hand.rank = 2;
-        } else { // high card
-            hand.rank = 1;
-        }
-    }
-
-    get_result(hands)
+    solve(input, 11)
 }
 
 #[allow(unused_variables)]
 fn part_2(input: &String) -> String {
-    let mut hands: Vec<Hand> = parse_input(input, &"1".to_string());
-    for hand in &mut hands {
-        let mut dict: HashMap<u64, u64> = HashMap::new();
-        let jokers: u64 = hand.cards.iter().filter(|c| **c == 1).count() as u64;
-        for card in hand.cards.iter() {
-            if *card != 1 {
-                dict.insert(*card, dict.get(card).unwrap_or(&0) + 1);
-            }
-        }
-
-        // get values from dict, not keys
-        let mut freq = dict.values()
-            .map(|v| *v)
-            .collect::<Vec<u64>>();
-        freq.push(0);
-        freq.push(0);
-        freq.sort();
-        freq.reverse();
-
-        if freq[0] + jokers == 5 { // 5 of a kind
-            hand.rank = 7;
-        } else if freq[0] + jokers == 4 { // 4 of a kind
-            hand.rank = 6;
-        } else if freq[0] + jokers == 3 && freq[1] == 2 { // full house
-            hand.rank = 5;
-        } else if freq[0] + jokers == 3 { // 3 of a kind
-            hand.rank = 4;
-        } else if freq[0] == 2 && freq[1] == 2 { // 2 pairs
-            hand.rank = 3;
-        } else if freq[0] + jokers == 2 { // 1 pair
-            hand.rank = 2;
-        } else { // high card
-            hand.rank = 1;
-        }
-    }
-
-    get_result(hands)
+    solve(input, 1)
 }
 
-fn parse_input(input: &String, j: &String) -> Vec<Hand> {
-    input
+fn solve(input: &String, j: u64) -> String {
+    let mut hands = input
         .lines()
-        .map(|line| line.to_string())
-        .map(|line| line.split(" ")
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>())
-        // convert index 1 to u64 and keep index 0
-        .map(|line| (
-            // iterate over string
-            line[0].chars()
-                .map(|c| if c == 'T' { "10".to_string() }
-                else if c == 'J' { j.to_string() }
-                else if c == 'Q' { "12".to_string() }
-                else if c == 'K' { "13".to_string() }
-                else if c == 'A' { "14".to_string() }
-                else { c.to_string() })
-                .map(|s| s.parse::<u64>().unwrap())
-                .collect::<Vec<u64>>(),
-            line[1].parse::<u64>().unwrap())
-        )
-        .map(|(cards, bid)| Hand { cards, bid, rank: 0 })
-        .collect::<Vec<Hand>>()
-}
+        .map(|line| {
+            let (cards, bid) = line.split_once(" ").unwrap();
+            let mut rank: u64 = 0;
 
-fn get_result(mut hands: Vec<Hand>) -> String {
-    hands.sort_by(|a, b| {
-        if a.rank == b.rank {
-            for i in 0..a.cards.len() {
-                if a.cards[i] != b.cards[i] {
-                    return a.cards[i].cmp(&b.cards[i]);
-                }
-            }
-            return a.cards[0].cmp(&b.cards[0]);
-        }
-        a.rank.cmp(&b.rank)
-    });
+            // let mut freq: HashMap<u64, u64> = HashMap::new();
+            let mut freq: u64 = 0;
+            let _ = cards.chars()
+                .map(|c| {
+                    let n = match c {
+                        'T' => 10,
+                        'J' => j,
+                        'Q' => 12,
+                        'K' => 13,
+                        'A' => 14,
+                        _ => c.to_string().parse::<u64>().unwrap()
+                    };
+                    freq += 1 << (n - 1) * 3;
+                    // freq.insert(n, freq.get(&n).unwrap_or(&0) + 1);
+                    rank = rank << 4 | n;
+                })
+                .collect::<Vec<_>>();
+            let bid = bid.parse::<u64>().unwrap();
 
-    let mut res: u64 = 0;
-    for i in 0..hands.len() {
-        res += hands[i].bid * (i as u64 + 1);
-    }
+            let jokers = freq & 0b111;
 
-    res.to_string()
+            let mut freq = (1 .. 14)
+                .map(|i| (freq >> (i * 3)) & 0b111)
+                .collect::<Vec<u64>>();
+
+            freq.sort();
+            freq.reverse();
+
+            rank |= match (freq[0] + jokers, freq[1]) {
+                (5, _) => 0x7_00000,
+                (4, _) => 0x6_00000,
+                (3, 2) => 0x5_00000,
+                (3, _) => 0x4_00000,
+                (2, 2) => 0x3_00000,
+                (2, _) => 0x2_00000,
+                _ => 0x1_00000,
+            };
+
+            Hand { bid, rank }
+        })
+        .collect::<Vec<Hand>>();
+
+    hands.sort_by_cached_key(|hand| hand.rank);
+
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, hand)| {
+            hand.bid * (i as u64 + 1)
+        })
+        .sum::<u64>()
+        .to_string()
 }
